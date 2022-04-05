37d2418e479ab2fe3dcf634d37f8974f541db6e1
Programming Hero <programming.hero1@gmail.com>
2022-04-04 22:40:05 -0500

    simple firebase login

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 import './Header.css';
 
 const Header = () => {
+    const {user} = useFirebase();
     return (
         <div className='header'>
             <nav>
@@ -10,7 +12,13 @@ const Header = () => {
                 <Link to="/products">Products</Link>
                 <Link to="/orders">Orders</Link>
                 <Link to="/register">Register</Link>
-                <Link to="/login">Login</Link>
+                {
+                    user.uid 
+                    ?
+                    <button>sign out</button>
+                    :
+                    <Link to="/login">Login</Link>
+                }
             </nav>
         </div>
     );
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,9 +1,20 @@
 import {useState} from 'react';
+import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import app from '../firebase.init';
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
 const useFirebase = () =>{
     const [user, setUser] = useState({});
 
     const singInWithGoogle = () =>{
-        console.log('singing in soon');
+        signInWithPopup(auth, googleProvider)
+        .then(result =>{
+            const user = result.user;
+            setUser(user);
+            console.log(user);
+        })
     }
     
     return {
