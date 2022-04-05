d2d5eded91e7628c9a820a2f5d8d3e84b75d31cf
Programming Hero <programming.hero1@gmail.com>
2022-04-05 00:31:00 -0500

    implemted protected Route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Login from './components/Login/Login';
 import Header from './components/Header/Header';
 import Register from './components/Register/Register';
 import Products from './components/Products/Products';
+import Orders from './components/Orders/Orders';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -16,6 +18,11 @@ function App() {
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders></Orders>
+          </RequireAuth>
+        }></Route>
       </Routes>
     </div>
   );
